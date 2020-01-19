b66800613237248bfc60f16e72a9f1d47bc2f19f
ncthanh2000 <ncthanh2000@yahoo.com>
2020-01-20 02:31:37 +0700

    Update

---
+++ package_01_score_optim.py
@@ -0,0 +1,63 @@
+import warnings
+
+import matplotlib.pyplot as plt
+# Classifier Libraries
+import scikitplot as skplt
+# Other Libraries
+from sklearn.model_selection import GridSearchCV
+
+warnings.filterwarnings("ignore")
+
+
+def score_optimization(_params, _clf, _metric,X_train, X_train_cv, y_train, y_train_cv, X_test, _cv):
+
+    if (_params != None):
+        # Load GridSearchCV
+        search = GridSearchCV(
+            estimator=_clf,
+            param_grid=_params,
+            n_jobs=-1,
+            scoring=_metric,
+            cv=_cv
+        )
+
+        # Train search object
+        search.fit(X_train, y_train)
+
+        # Heading
+        print('\n', '-' * 40, '\n', _clf.__class__.__name__, '\n', '-' * 40)
+
+        # Extract best estimator
+        best = search.best_estimator_
+        print('Best parameters: \n\n', search.best_params_, '\n')
+
+        # Cross-validate on the train data
+        #Uncomment this to see how the best estimator performs, rather than just knowing what best estimator is
+        # Maybe change the cv to something smaller
+        # Now predict on X_train_cv
+        predicted_probas = best.predict_proba(X_train_cv)
+        y_true = y_train_cv
+        y_probas = predicted_probas
+        skplt.metrics.plot_roc_curve(y_true, y_probas)
+        plt.show()
+
+        # Now predict on X_test
+        return best.predict_proba(X_test)[:, 1]
+    else:
+        #Run with defaul parameters
+        _clf.fit(X_train, y_train)
+        # Heading
+        print('\n', '-' * 40, '\n', _clf.__class__.__name__, '\n', '-' * 40)
+        predicted_probas = _clf.predict_proba(X_train_cv)
+        y_true = y_train_cv
+        y_probas = predicted_probas
+        skplt.metrics.plot_roc_curve(y_true, y_probas)
+        plt.show()
+
+        # Now predict on X_test
+        return _clf.predict_proba(X_test)[:, 1]
+
+
+
+
+
